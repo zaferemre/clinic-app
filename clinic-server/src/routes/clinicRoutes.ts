7751c944c399023a188972de7b7503fb9fd6488a
zaferemre <emrecalisir34@gmail.com>
2025-06-26 21:49:48 +0300

    acces clinic

--- clinic-server/src/routes/clinicRoutes.ts
+++ clinic-server/src/routes/clinicRoutes.ts
@@ -4,11 +4,12 @@ import { verifyFirebaseToken } from "../middlewares/verifyFirebaseToken";
 import { authorizeCompanyAccess } from "../middlewares/authorizeCompanyAccess";
 
 const router = Router({ mergeParams: true });
-router.use(verifyFirebaseToken, authorizeCompanyAccess);
+router.use(verifyFirebaseToken);
 
 router.get("/", clinicCtrl.listClinics);
 router.post("/", clinicCtrl.createClinic);
 router.get("/:clinicId", clinicCtrl.getClinicById);
+router.use(authorizeCompanyAccess);
 router.patch("/:clinicId", clinicCtrl.updateClinic);
 router.delete("/:clinicId", clinicCtrl.deleteClinic);
 
